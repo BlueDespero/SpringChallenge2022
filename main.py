3281a8c46323f06e53cfbfad1d8479bf14f14d4a
BlueDespero <cezarytroska@gmail.com>
2022-05-02 00:01:23 +0200

    Use wind

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import sys
 import numpy as np
 
 base_x, base_y = [int(i) for i in input().split()]
+enemy_base_x, enemy_base_y = [0, 0] if base_x != 0 else [17630, 9000]
+
 heroes_per_player = int(input())  # Always 3
 heroes = []
 
@@ -24,9 +26,9 @@ def distance(x1, y1, x2, y2):
 
 def get_default_position():
     if base_x == 0:
-        positions = [(1200, 5000), (3500, 3500), (5000, 600)]
+        positions = [(1469, 5600), (4254, 3900), (6200, 900)]
     else:
-        positions = [(12000, 8000), (14000, 6000), (17000, 4000)]
+        positions = [(11834, 7447), (13387, 4757), (16077, 3204)]
     for i in range(3):
         yield positions[i]
 
@@ -126,6 +128,50 @@ def get_turns_distance(hero: Hero, monster: Monster):
     return counter
 
 
+def get_base_turns_distance(monster: Monster):
+    counter = 1
+    dist = distance(base_x, base_y, monster.x + monster.vx * counter, monster.y + monster.vy * counter)
+
+    while dist >= 5000:
+        counter += 1
+        dist = distance(base_x, base_y, monster.x + monster.vx * counter, monster.y + monster.vy * counter)
+
+    sim_x, sim_y = monster.x + monster.vx * counter, monster.y + monster.vy * counter
+    counter += (distance(base_x, base_y, sim_x, sim_y) - 300) / 400
+
+    return counter
+
+
+def can_catch_kill(hero: Hero, monster: Monster):
+    mon_time = get_base_turns_distance(monster)
+    catch_time = get_turns_distance(hero, monster)
+    kill_time = monster.health / 2
+    return mon_time >= (catch_time + kill_time)
+
+
+def should_wind_attack(hero: Hero, monster: Monster):
+    if mana < 10:
+        return False
+
+    if distance(hero.x, hero.y, monster.x, monster.y) > 1280:
+        return False
+
+    if distance(base_x, base_y, monster.x + monster.vx, monster.y + monster.vy) <= 5500:
+        return True
+
+    #if not can_catch_kill(hero, monster):
+    #    return True
+
+    return False
+
+
+def attack(hero: Hero, monster: Monster):
+    if should_wind_attack(hero, monster):
+        print("SPELL WIND %s %s" % (enemy_base_x, enemy_base_y))
+    else:
+        print("MOVE %s %s" % (monster.x + monster.vx, monster.y + monster.vy))
+
+
 def match_heroes_targets(targets):
     all_perms = itertools.permutations(range(3))
     targets = [t[1] for t in targets]
@@ -147,7 +193,7 @@ def match_heroes_targets(targets):
     for i, p in enumerate(best_perm):
         if len(targets) > best_perm.index(i):
             target = targets[best_perm.index(i)]
-            print("MOVE %s %s" % (target.x + target.vx, target.y + target.vy))
+            attack(heroes[i], target)
         else:
             print("MOVE %s %s" % (heroes[i].def_x, heroes[i].def_y))
 
@@ -189,13 +235,3 @@ while True:
     targets = heapq.nsmallest(min(heroes_per_player, len(monsters)), monsters)
 
     match_heroes_targets(targets)
-    '''
-    for i in range(heroes_per_player):
-        if targets:
-            target = targets.pop()[1]
-            # print("%s %s %s %s  || %s %s" % (target.x, target.vx, target.y, target.vy, target.x + target.vx,
-            # target.y + target.vy), file=sys.stderr)
-            print("MOVE %s %s" % (target.x + target.vx, target.y + target.vy))
-        else:
-            print("MOVE %s %s" % (heroes[i].def_x, heroes[i].def_y))
-    '''
